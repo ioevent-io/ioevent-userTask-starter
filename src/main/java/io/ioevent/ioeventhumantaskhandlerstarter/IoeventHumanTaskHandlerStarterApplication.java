d1624f34e7ebc266330dd05aebf91a3834ae8aab
jesser <jesser.abidelli@codeonce.fr>
2023-12-26 13:01:46 +0100

    first commit

---
+++ src/main/java/io/ioevent/ioeventhumantaskhandlerstarter/IoeventHumanTaskHandlerStarterApplication.java
@@ -0,0 +1,13 @@
+package io.ioevent.ioeventhumantaskhandlerstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoeventHumanTaskHandlerStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoeventHumanTaskHandlerStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ioevent/ioeventhumantaskhandlerstarter/IoeventHumanTaskHandlerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ioevent.ioeventhumantaskhandlerstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoeventHumanTaskHandlerStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
